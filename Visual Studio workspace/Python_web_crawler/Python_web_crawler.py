c7e512844687677d4e153bda22e5d4d5e88af50f
Chen Hank <arsf32303@gmail.com>
2021-11-18 01:21:11 +0800

    upload

--- Visual Studio workspace/Python_web_crawler/Python_web_crawler.py
+++ Visual Studio workspace/Python_web_crawler/Python_web_crawler.py
@@ -1,19 +1,48 @@
-from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.keys import Keys as key
-import time,re
+#main file
+from import_ import *
 
-driver= webdriver.Chrome()
+clean_file("./information.py")
+driver= webdriver.Firefox()
 driver.get('https://wateroff.water.gov.tw/city/%E8%87%BA%E4%B8%AD%E5%B8%82/index.html')
-location_position = WebDriverWait(driver,10).until(EC.presence_of_element_located((By.CLASS_NAME, "text-input")))
-location_position.send_keys('東區')
-time.sleep(5)
-location_position.send_keys(key.ENTER)
 
-case_num = driver.find_elements_by_xpath('/html/body/main/div/div[2]/div[1]/div/div[4]/a/div[2]/div[2]/div[2]/div')
-for num in case_num:
-	print("Result: ",num.text)
+#選擇篩選區域  南區: 66000030 北區: 66000050 南屯區: 66000070
+locate_place(driver, '/html/body/main/div/div[1]/div[2]/div[1]/select', '66000030', 'citySelect')
+sleep(2)
+case_num = get_case_num(driver, "/html/body/main/div/div[2]/div[1]/div/div/a/div[2]/div[2]/div[2]/div")
+
+#導向停水的詳細資訊
+locate_url = "https://wateroffmap.water.gov.tw/map/view/" + case_num
+driver.get(locate_url)
+sleep(1)
+
+#寫入檔案
+writefile(driver, 'start_water_cut_date', '/html/body/div/div/div[1]/div[3]/div[2]/div[2]/div[1]/div[1]', '')
+writefile(driver, 'start_water_cut_time', '/html/body/div/div/div[1]/div[3]/div[2]/div[2]/div[1]/div[2]/span', '')
+
+writefile(driver, 'stop_water_cut_date', '/html/body/div/div/div[1]/div[3]/div[2]/div[2]/div[2]/div[1]', '')
+writefile(driver, 'stop_water_cut_time', '/html/body/div/div/div[1]/div[3]/div[2]/div[2]/div[2]/div[2]/span', '')
+
+writefile(driver, 'water_cut_range', '/html/body/div/div/div[1]/div[3]/div[2]/div[3]/div[1]/div[2]', '')
+writefile(driver, 'water_cut_reason', '/html/body/div/div/div[1]/div[3]/div[2]/div[3]/div[2]/div[2]', '')
+
+writefile(driver, 'post_time', '/html/body/div/div/div[1]/div[3]/div[2]/div[5]/div[4]/div', '')
+writefile(driver, 'water_cut_tips', '/html/body/div/div/div[1]/div[3]/div[2]/div[5]/div[7]/div', '')
+url = "https://wateroffmap.water.gov.tw/map/view/" + case_num
+writefile(driver, 'more_information', '', url)
+
+print("\n")
+
+from information import *
+sleep(0.5)
+print(f"停水日期: {start_water_cut_date}")
+print(f"停水時間: {start_water_cut_time}")
+print(f"供水日期: {stop_water_cut_date}")
+print(f"供水時間: {stop_water_cut_time}")
+print(f"停水範圍: {water_cut_range}")
+print(f"停水原因: {water_cut_reason}")
+print(f"發布日期: {post_time}")
+print(f"停水叮嚀: {water_cut_tips}")
+print(f"更多資訊: {more_information}")
+
 input()
+
---
+++ Visual Studio workspace/Python_web_crawler/import_.py
@@ -0,0 +1,10 @@
+#import 在這裡
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys as key
+from selenium.webdriver.support.ui import Select
+import time, re
+from function_ import *
