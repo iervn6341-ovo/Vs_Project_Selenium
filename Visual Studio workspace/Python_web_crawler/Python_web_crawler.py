de4b75866f6505b5511ae1f7f3513e6bfd39acdc
Chen Hank <arsf32303@gmail.com>
2021-11-18 23:59:56 +0800

    upload

--- Visual Studio workspace/Python_web_crawler/Python_web_crawler.py
+++ Visual Studio workspace/Python_web_crawler/Python_web_crawler.py
@@ -1,14 +1,32 @@
-#main file
+#Main file
 from import_ import *
 
 clean_file("./information.py")
+count_div = 1
+
 driver= webdriver.Firefox()
 driver.get('https://wateroff.water.gov.tw/city/%E8%87%BA%E4%B8%AD%E5%B8%82/index.html')
 
 #選擇篩選區域  南區: 66000030 北區: 66000050 南屯區: 66000070
-locate_place(driver, '/html/body/main/div/div[1]/div[2]/div[1]/select', '66000030', 'citySelect')
-sleep(2)
-case_num = get_case_num(driver, "/html/body/main/div/div[2]/div[1]/div/div/a/div[2]/div[2]/div[2]/div")
+try:
+	locate_place(driver, '/html/body/main/div/div[1]/div[2]/div[1]/select', '66000030', 'citySelect')
+except NoSuchElementError:
+	print("找不到網頁元素")
+
+while(1) :
+	if  count_div == 21 :
+		break
+
+	path = "/html/body/main/div/div[2]/div[1]/div/div[" + str(count_div) + "]/a/div[2]/div[2]/div[2]/div"
+
+	try:
+		case_num = get_case_num(driver, path)
+	except:
+		break
+	count_div += 1
+
+#/html/body/main/div/div[2]/div[1]/div/div[1]/a/div[2]/div[2]/div[2]/div
+#/html/body/main/div/div[2]/div[1]/div/div[2]/a/div[2]/div[2]/div[2]/div
 
 #導向停水的詳細資訊
 locate_url = "https://wateroffmap.water.gov.tw/map/view/" + case_num
@@ -30,10 +48,9 @@ writefile(driver, 'water_cut_tips', '/html/body/div/div/div[1]/div[3]/div[2]/div
 url = "https://wateroffmap.water.gov.tw/map/view/" + case_num
 writefile(driver, 'more_information', '', url)
 
-print("\n")
-
 from information import *
-sleep(0.5)
+
+print("\n")
 print(f"停水日期: {start_water_cut_date}")
 print(f"停水時間: {start_water_cut_time}")
 print(f"供水日期: {stop_water_cut_date}")
