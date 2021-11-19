0d409012b4e8784ec94e76a45c8ecbf4be19d322
user8171 <90666894+user8171@users.noreply.github.com>
2021-11-19 16:38:26 +0800

    Add files via upload

--- Python_web_crawler/Python_web_crawler.py
+++ Python_web_crawler/Python_web_crawler.py
@@ -2,20 +2,22 @@
 from import_ import *
 
 clean_file("./information_.py")
+count_div = 1
+
 driver= webdriver.Firefox()
 driver.get('https://wateroff.water.gov.tw/city/%E8%87%BA%E4%B8%AD%E5%B8%82/index.html')
 
-#選擇篩選區域  南區: 66000030 北區: 66000050 南屯區: 66000070
-locate_place(driver, '/html/body/main/div/div[1]/div[2]/div[1]/select', '66000030', 'citySelect')
-sleep(2)
-case_num = get_case_num(driver, "/html/body/main/div/div[2]/div[1]/div/div/a/div[2]/div[2]/div[2]/div")
+#選擇篩選區域  南區: 66000030 北區: 66000050 南屯區: 66000070 西屯區: 66000060
+#北屯區: 66000080
+#若沒有請註解
+#locate_place(driver, '/html/body/main/div/div[1]/div[2]/div[1]/select', '66000070', 'citySelect')
 
 while(1) :
 	if  count_div == 21 :
 		break
 
 	path = "/html/body/main/div/div[2]/div[1]/div/div[" + str(count_div) + "]/a/div[2]/div[2]/div[2]/div"
-
+	
 	try:
 		#取得案件編號
 		case_num = get_case_num(driver, path)
@@ -25,11 +27,20 @@ while(1) :
 		driver.get(locate_url)
 
 		#寫入檔案
-		collect_information(driver, case_num)
+		if [ driver_wait_by_xpath_text(driver, '/html/body/div/div/div[1]/div[3]/div[2]/div[3]/div[1]/div[2]') != '無停水地區' and driver_wait_by_xpath_text(driver, '/html/body/div/div/div[1]/div[3]/div[2]/div[4]/div[1]/div[2]') != '無降壓地區' ]:
+			collect_information_condition_1(driver, case_num)
+		elif [ driver_wait_by_xpath_text(driver, '/html/body/div/div/div[1]/div[3]/div[2]/div[3]/div') == '無停水地區' ] :
+			collect_information_condition_2(driver, case_num)
+
+		elif [ driver_wait_by_xpath_text(driver, '/html/body/div/div/div[1]/div[3]/div[2]/div[4]/div') == '無降壓地區' ] :
+			collect_information_condition_3(driver, case_num)	
+		else:
+			collect_information_condition_4(driver, case_num)
 
 		#印出資訊
 		from information_ import *
 		print("\n")
+		print(f"案件編號: {case_number}")
 		print(f"停水日期: {start_water_cut_date}")
 		print(f"停水時間: {start_water_cut_time}")
 		print(f"供水日期: {stop_water_cut_date}")
@@ -40,11 +51,14 @@ while(1) :
 		print(f"停水叮嚀: {water_cut_tips}")
 		print(f"更多資訊: {more_information}")
 		print("\n")
+		driver.back()
 	except Exception as e:
+		print(e)
 		print("似乎是最後一筆了呢")
-	except:
-		print("發生未知的錯誤")
+		break
+	#except:
+	#	print("發生未知的錯誤")
+	#	break
 	count_div += 1
 
-input()
-
+driver.quit()
--- Python_web_crawler/function_.py
+++ Python_web_crawler/function_.py
@@ -9,19 +9,25 @@ def clean_file(path):
 	file.close()
 
 #利用webdriverwait函式等待直到找到指定xpath元素
-def driverwaitbyxpath(driver, x_path):
+def driver_wait_by_xpath(driver, x_path):
 	information = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, x_path)))
+	return information
+
+def driver_wait_by_xpath_text(driver, x_path):
+	information = driver_wait_by_xpath(driver, x_path)
+	information = information.text
 	return information
 
 #選取行政區
 def locate_place(driver, x_path, value, id_):
 	#location_position = Select(driver.find_element_by_id(id_))
 	path = x_path + '/option[@value=' + value + ']'
-	driverwaitbyxpath(driver, '/html/body/main/div/div[2]/div[1]/div/div[1]/a')
-	driverwaitbyxpath(driver, path).click()
+	driver_wait_by_xpath(driver, '/html/body/main/div/div[2]/div[1]/div/div[1]/a')
+	driver_wait_by_xpath(driver, path).click()
 
 #取得案件編號
 def get_case_num(driver, x_path):
+	driver_wait_by_xpath(driver, '/html/body/main/div/div[2]/div[1]/div/div[1]/a')
 	case_num = driver.find_element_by_xpath(x_path).text
 	return case_num
 
@@ -30,32 +36,76 @@ def sleep(times):
 	time.sleep(times)
 
 #將當前案件編號的資訊蒐集起來
-def collect_information(driver, case__num):
+#collect_information_condition_1: 有停水有降壓地區尚未復水的狀態
+#collect_information_condition_2: 無停水有降壓地區尚未復水的狀態
+#collect_information_condition_3: 有停水無降壓地區尚未復水的狀態
+#collect_information_condition_4: 已復水
+
+def collect_information_condition_1(driver, case__num):
 	writefile(driver, 'case_number ', '', case__num)
 	writefile(driver, 'start_water_cut_date', '/html/body/div/div/div[1]/div[3]/div[2]/div[2]/div[1]/div[1]', '')
-	#/html/body/div/div/div[1]/div[3]/div[2]/div[2]/div[1]/div[1]
-	#/html/body/div/div/div[1]/div[3]/div[2]/div[2]/div[2]/div[1]
-	#/html/body/div/div/div[1]/div[3]/div[2]/div[2]/div[2]/div[1]
+	writefile(driver, 'start_water_cut_time', '/html/body/div/div/div[1]/div[3]/div[2]/div[2]/div[1]/div[2]/span', '')
+
+	writefile(driver, 'stop_water_cut_date', '/html/body/div/div/div[1]/div[3]/div[2]/div[2]/div[2]/div[1]', '')
+	writefile(driver, 'stop_water_cut_time', '/html/body/div/div/div[1]/div[3]/div[2]/div[2]/div[2]/div[2]/span', '')
+	writefile(driver, 'water_cut_range', '/html/body/div/div/div[1]/div[3]/div[2]/div[3]/div[1]/div[2]', '')
+	writefile(driver, 'water_cut_reason', '/html/body/div/div/div[1]/div[3]/div[2]/div[3]/div[2]/div[2]', '')
+	writefile(driver, 'low_water_range', '/html/body/div/div/div[1]/div[3]/div[2]/div[4]/div[1]/div[2]', '')
+	writefile(driver, 'low_water_reason', '/html/body/div/div/div[1]/div[3]/div[2]/div[4]/div[2]/div[2]', '')
+	writefile(driver, 'post_time', '/html/body/div/div/div[1]/div[3]/div[2]/div[5]/div[4]/div', '')
+	writefile(driver, 'water_cut_tips', '/html/body/div/div/div[1]/div[3]/div[2]/div[5]/div[7]/div', '')
+	writefile(driver, 'status', '', '未復水')
+	url = "https://wateroffmap.water.gov.tw/map/view/" + case__num
+	writefile(driver, 'more_information', '', url)
+
+def collect_information_condition_2(driver, case__num):
+	writefile(driver, 'case_number ', '', case__num)
+	writefile(driver, 'start_water_cut_date', '/html/body/div/div/div[1]/div[3]/div[2]/div[2]/div[1]/div[1]', '')
 	writefile(driver, 'start_water_cut_time', '/html/body/div/div/div[1]/div[3]/div[2]/div[2]/div[1]/div[2]/span', '')
 
 	writefile(driver, 'stop_water_cut_date', '/html/body/div/div/div[1]/div[3]/div[2]/div[2]/div[2]/div[1]', '')
 	writefile(driver, 'stop_water_cut_time', '/html/body/div/div/div[1]/div[3]/div[2]/div[2]/div[2]/div[2]/span', '')
 
-	writefile(driver, 'water_cut_range', '/html/body/div/div/div[1]/div[3]/div[2]/div[3]/div[1]/div[2]', '')
-	writefile(driver, 'water_cut_reason', '/html/body/div/div/div[1]/div[3]/div[2]/div[3]/div[2]/div[2]', '')
-
+	writefile(driver, 'water_cut_range', '', 'None')
+	writefile(driver, 'water_cut_reason', '', 'None')
+	writefile(driver, 'low_water_range', '/html/body/div/div/div[1]/div[3]/div[2]/div[4]/div[1]/div[2]', '')
+	writefile(driver, 'low_water_reason', '/html/body/div/div/div[1]/div[3]/div[2]/div[4]/div[2]/div[2]', '')
 	writefile(driver, 'post_time', '/html/body/div/div/div[1]/div[3]/div[2]/div[5]/div[4]/div', '')
 	writefile(driver, 'water_cut_tips', '/html/body/div/div/div[1]/div[3]/div[2]/div[5]/div[7]/div', '')
+	writefile(driver, 'status', '', '未復水')
 	url = "https://wateroffmap.water.gov.tw/map/view/" + case__num
 	writefile(driver, 'more_information', '', url)
 
+def collect_information_condition_3(driver, case__num):
+	writefile(driver, 'case_number ', '', case__num)
+	writefile(driver, 'start_water_cut_date', '/html/body/div/div/div[1]/div[3]/div[2]/div[2]/div[1]/div[1]', '')
+	writefile(driver, 'start_water_cut_time', '/html/body/div/div/div[1]/div[3]/div[2]/div[2]/div[1]/div[2]/span', '')
+
+	writefile(driver, 'stop_water_cut_date', '/html/body/div/div/div[1]/div[3]/div[2]/div[2]/div[2]/div[1]', '')
+	writefile(driver, 'stop_water_cut_time', '/html/body/div/div/div[1]/div[3]/div[2]/div[2]/div[2]/div[2]/span', '')
+	writefile(driver, 'water_cut_range', '/html/body/div/div/div[1]/div[3]/div[2]/div[3]/div[1]/div[2]', '')
+	writefile(driver, 'water_cut_reason', '/html/body/div/div/div[1]/div[3]/div[2]/div[3]/div[2]/div[2]', '')
+	writefile(driver, 'low_water_range', '', 'None')
+	writefile(driver, 'low_water_reason', '', 'None')
+	writefile(driver, 'post_time', '/html/body/div/div/div[1]/div[3]/div[2]/div[5]/div[4]/div', '')
+	writefile(driver, 'water_cut_tips', '/html/body/div/div/div[1]/div[3]/div[2]/div[5]/div[7]/div', '')
+	writefile(driver, 'status', '', '未復水')
+	url = "https://wateroffmap.water.gov.tw/map/view/" + case__num
+	writefile(driver, 'more_information', '', url)
+
+def collect_information_condition_4(driver, case__num):
+	writefile(driver, 'case_number ', '', case__num)
+	writefile(driver, 'status', '', '已復水')
+	url = "https://wateroffmap.water.gov.tw/map/view/" + case__num
+	writefile(driver, 'more_information', '', url)
+#
 #parameter_name: 傳至 [information.py] 的變數名稱 , x_path: 搜尋網頁元素的路徑
-def writefile(driver, parameter_name, x_path, url):
+def writefile(driver, parameter_name, x_path, output_text):
 	file = open('./information_.py', 'a')
 	if x_path != '':
 		catch_text = driver.find_element_by_xpath(x_path).text
 		text_temp = parameter_name + " = '" + catch_text + "'\n"
 		file.write(text_temp)
-	if url != '':
-		file.write(parameter_name + " = '" + url + "'\n")
+	if output_text != '':
+		file.write(parameter_name + " = '" + output_text + "'\n")
 	file.close()
